5da3b594507dd40459d2042a9b59d95eba029ce8
davxrob92 <davxrob92@icloud.com>
2025-09-29 11:23:37 -0500

    Ch3 Report, L2: Arguments

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # this function takes a filepath as input and returns the contents of a file as a string
+from stats import count_book_words, get_book_characters, sorted_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f: 
         return f.read()
-
-def count_book_words(text):
-    return len(text.split())
-
         
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_book_words(book_text)
+    character_count = get_book_characters("books/frankenstein.txt")
+    sortied = sorted_dict(character_count)
+    print("============ BOOKBOT ============")
     print(f"Found {num_words} total words")
+    print("--------- Character Count ------- ")
+    for s in sortied:
+        if s["char"].isalpha() == True:
+            print(s["char"]+":",s["num"])
+    print("============= END ===============")
 main()
